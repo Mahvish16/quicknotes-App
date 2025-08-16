eabe6699d3a056cad3f4774de13349a0a4cd0931
Mahvish16 <mahvish.ruhi@gmail.com>
2025-08-16 14:39:12 +0000

    update settings and add login url

--- quicknotes/notes/urls.py
+++ quicknotes/notes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView,LoginView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -8,5 +8,6 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='Register'),
+    path('login/', LoginView.as_view(), name='Login'),
     
 ]
--- quicknotes/quicknotes/settings.py
+++ quicknotes/quicknotes/settings.py
@@ -11,20 +11,23 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import environ
 import os
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
-env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
